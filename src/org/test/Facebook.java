b267aeee6eb5d1e4f0fa07619054a1a7a752699b
VenkataPrasana <venkata.prasana@gmail.com>
2022-01-01 10:33:32 +0530

    Folder structure

---
+++ src/org/test/Facebook.java
@@ -0,0 +1,12 @@
+package org.test;
+
+public class Facebook {
+private void user() {
+	System.out.println("Welcome to facebook");
+
+}
+public static void main(String[] args) {
+	Facebook book=new Facebook();
+	book.user();
+}
+}
